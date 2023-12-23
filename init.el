05affabc01cd0d43db65bcda8078d57b8c6c9ac5
JJ <jaroslaw.janikowski@gmail.com>
2023-12-23 22:38:51 +0100

    Dodanie csv-mode.

--- init.el
+++ init.el
@@ -548,6 +548,7 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.env\\..*\\'" . dotenv-mode))
 (add-to-list 'auto-mode-alist '("\\.service" . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.csv" . csv-mode))
 (add-to-list 'auto-mode-alist '("README" . text-mode))
 (add-to-list 'auto-mode-alist '("LICENSE" . text-mode))
 (add-to-list 'auto-mode-alist '("CHANGELOG" . text-mode))
