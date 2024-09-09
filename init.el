2f07a00ee6443e7e4b72750dbad7ff65a45bc8ee
JJ <jaroslaw.janikowski@gmail.com>
2024-09-09 23:42:45 +0200

    Dodanie obsługi edycji dla plików SVG.

--- init.el
+++ init.el
@@ -766,6 +766,7 @@
 (add-to-list 'auto-mode-alist '("LICENSE$" . text-mode))
 (add-to-list 'auto-mode-alist '("CHANGELOG$" . text-mode))
 (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
+(add-to-list 'auto-mode-alist '("\\.svg$" . nxml-mode))
 
 (add-to-list 'dape-configs `(debugpy
 							 modes (python-ts-mode python-mode)
