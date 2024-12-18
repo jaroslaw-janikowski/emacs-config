1f65fd9d5a1fd2879c424927b3799b551f467d26
JJ <jaroslaw.janikowski@gmail.com>
2024-12-18 15:30:12 +0100

    Dodanie instalacji gdb.

--- install/setup-debian-base.sh
+++ install/setup-debian-base.sh
@@ -1,4 +1,4 @@
-apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock qemu-system-x86 grimshot wdisplays p7zip-full xterm clangd nsis gnupg2 gnuplot gnuplot-doc graphviz graphviz-doc unifont silversearcher-ag libtree-sitter-dev
+apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock qemu-system-x86 grimshot wdisplays p7zip-full xterm clangd nsis gnupg2 gnuplot gnuplot-doc graphviz graphviz-doc unifont silversearcher-ag libtree-sitter-dev gdb
 
 # spelling tools
 apt install -y ispell ipolish ibritish iamerican
