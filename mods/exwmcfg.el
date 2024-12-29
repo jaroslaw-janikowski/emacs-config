9e1ce77454dfa2248c7a862ec340cc70d49f43a4
JJ <jaroslaw.janikowski@gmail.com>
2024-12-29 12:48:00 +0100

    Fix for screen resize event.

--- mods/exwmcfg.el
+++ mods/exwmcfg.el
@@ -4,14 +4,14 @@
 
 (defun my-exwm-update-title ()
   (pcase exwm-class-name
-	("Firefox" (exwm-workspace-rename-buffer (format "Firefox: %s" exwm-title)))))
+    ("Firefox" (exwm-workspace-rename-buffer (format "Firefox: %s" exwm-title)))))
 
-(add-hook 'exwm-randr-screen-change-hook
-		  (lambda ()
-			(message "Resizing screen...")  ;; Do not remove. Resize does not work without this line :P
-			(exwm-randr-refresh)
-			(message "Done.")))
+(defun my-exwm-update-screen ()
+  (message "Resizing screen...")  ;; Do not remove. Resize does not work without this line :P
+  (exwm-randr-refresh)
+  (message "Done."))
 
+(add-hook 'exwm-randr-screen-change-hook #'my-exwm-update-screen)
 (add-hook 'exwm-update-title-hook #'my-exwm-update-title)
 
 (exwm-enable)
