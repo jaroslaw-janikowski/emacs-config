0999799b92f3987f19cc37904af0f7c50486137d
JJ <jaroslaw.janikowski@gmail.com>
2025-01-19 11:11:27 +0100

    Dodanie instalacji użytecznego narzędzia.

--- install/setup-debian-base.sh
+++ install/setup-debian-base.sh
@@ -1,4 +1,4 @@
-apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock qemu-system-x86 grimshot wdisplays p7zip-full xterm clangd nsis gnupg2 gnuplot gnuplot-doc graphviz graphviz-doc unifont silversearcher-ag libtree-sitter-dev gdb
+apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock qemu-system-x86 grimshot wdisplays p7zip-full xterm clangd nsis gnupg2 gnuplot gnuplot-doc graphviz graphviz-doc unifont silversearcher-ag libtree-sitter-dev gdb clzip
 
 # spelling tools
 apt install -y aspell aspell-pl aspell-en
