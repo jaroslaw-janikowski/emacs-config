76c1fe907e46c35bf631424f81a61d0fe1e224bf
JJ <jaroslaw.janikowski@gmail.com>
2025-04-01 09:47:15 +0200

    Wymiana terminala.

--- install/setup-debian-base.sh
+++ install/setup-debian-base.sh
@@ -4,7 +4,7 @@ apt upgrade -y
 apt autoremove -y
 
 # instalacja narzędzi
-apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock qemu-system-x86 grimshot wdisplays p7zip-full xterm clangd gnupg2 unifont silversearcher-ag gdb clzip sqlite3 sqlite3-doc
+apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock qemu-system-x86 grimshot wdisplays p7zip-full stterm clangd gnupg2 unifont silversearcher-ag gdb clzip sqlite3 sqlite3-doc
 
 # nagrywanie ekranu w Wayland
 apt install -y wf-recorder
