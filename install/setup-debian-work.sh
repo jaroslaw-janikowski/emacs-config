6e57a97f64146e1e435be233ea5ca1ecf44c6c14
JJ <jaroslaw.janikowski@gmail.com>
2025-07-21 10:28:59 +0200

    Poprawki w instalatorze laptopa do pracy.

--- install/setup-debian-work.sh
+++ install/setup-debian-work.sh
@@ -3,16 +3,6 @@ apt update -y
 apt upgrade -y
 apt autoremove -y
 
-# desktop
-apt install -y xorg openbox pcmanfm pavucontrol p7zip-full stterm clangd gnupg2 unifont gdb clzip sqlite3 sqlite3-doc brightnessctl dmenu
-su - work -- <<EOF
-mkdir -p ~/.config/openbox
-echo 'exec openbox-session' >> ~/.xinitrc &&
-echo '[ "$(tty)" = "/dev/tty1" ] && exec startx' >> ~/.profile
-echo 'brightnessctl set 20% &' >> ~/.config/openbox/autostart
-echo 'pcmanfm --desktop &' >> ~/.config/openbox/autostart
-EOF
-
 # przeglądarki internetowe do webdevu
 su - work -- <<EOF
 cd /tmp
@@ -30,10 +20,8 @@ apt install -y git git-gui
 # ufw setup
 apt install -y ufw
 systemctl enable ufw
-su - root -- <<EOF
 ufw deny telnet
 ufw deny vnc
-EOF
 
 # emacs
 apt build-dep -y emacs
@@ -42,7 +30,7 @@ su - work -- <<EOF
 rm -rf ~/.emacs.d
 rm ~/.emacs
 git clone --depth 1 https://github.com/jaroslaw-janikowski/emacs-config ~/.emacs.d
-sed -i "s/;; (require 'work)/(require 'work)/"
+sed -i "s/;; (require 'work)/(require 'work)/" ~/.emacs.d/init.el
 git clone --depth 1 https://git.savannah.gnu.org/git/emacs.git ~/workspace/emacs
 cd ~/workspace/emacs && make bootstrap
 EOF
@@ -50,7 +38,6 @@ cd /home/work/emacs && make install
 su - work -- <<EOF
 emacs -Q --script ~/.emacs.d/install/setup-emacs.el
 emacs -Q --script ~/.emacs.d/install/setup-emacs-work.el
-echo 'emacs &' >> ~/.config/openbox/autostart
 EOF
 
 # nsis
@@ -137,4 +124,62 @@ EOF
 su - work -- <<EOF
 ollama pull deepseek-r1:7b
 ollama pull qwen2.5-coder
+EOF
+
+# # desktop
+# # Na końcu aby nie psuć wykonywania skryptów jako user work
+# apt install -y xorg openbox pcmanfm pavucontrol p7zip-full stterm clangd gnupg2 unifont gdb clzip sqlite3 sqlite3-doc brightnessctl dmenu
+# su - work -- <<EOF
+# mkdir -p ~/.config/openbox
+# echo 'exec openbox-session' >> ~/.xinitrc &&
+# echo '[ "$(tty)" = "/dev/tty1" ] && exec startx' >> ~/.profile
+# echo 'brightnessctl set 20% &' >> ~/.config/openbox/autostart
+# echo 'pcmanfm --desktop &' >> ~/.config/openbox/autostart
+# EOF
+
+# Sway desktop
+apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock grimshot wdisplays p7zip-full stterm clangd gnupg2 unifont silversearcher-ag gdb clzip sqlite3 sqlite3-doc
+
+# suckless terminal
+apt install -y stterm
+update-alternatives --config x-terminal-emulator
+
+# nagrywanie ekranu w Wayland
+apt install -y wf-recorder
+
+# spelling tools
+apt install -y aspell aspell-pl aspell-en
+
+# sway setup
+su - work -- <<EOF
+mkdir ~/.config/sway
+cp /etc/sway/config ~/.config/sway/config
+cat <<EOF2>> ~/.config/sway/config
+
+set \$term st
+
+exec brightnessctl set 6%
+
+input * {
+    xkb_layout "pl"
+}
+
+xwayland enable
+bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +10%
+bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -10%
+bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle
+bindsym XF86MonBrightnessUp exec brightnessctl set 3%+
+bindsym XF86MonBrightnessDown exec brightnessctl set 3%-
+bindsym Print exec grimshot copy area
+workspace_layout tabbed
+
+EOF2
+
+sed -i "s/position top/position bottom" ~/.config/sway/config
+EOF
+
+cat <<EOF>> ~/.profile
+
+sway &
+
 EOF
