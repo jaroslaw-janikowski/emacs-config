f6dc03dd864d3727b004768463952e52bd57b7ad
JJ <jaroslaw.janikowski@gmail.com>
2025-07-24 13:26:25 +0200

    Dodanie narzędzi Sublime.

--- install/setup-debian-work.sh
+++ install/setup-debian-work.sh
@@ -194,3 +194,9 @@ EOF
 # sway &
 #
 # EOF
+
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | tee /etc/apt/sources.list.d/sublime-text.sources
+apt -y update
+apt install -y sublime-text sublime-merge
