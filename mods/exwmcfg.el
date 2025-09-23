15b65581e612cb3dccf3c2e68d4f6c2c937ee447
JJ <jaroslaw.janikowski@gmail.com>
2025-09-23 21:19:14 +0200

    Lepsze tytuły okien.

--- mods/exwmcfg.el
+++ mods/exwmcfg.el
@@ -3,8 +3,7 @@
 (require 'exwm)
 
 (defun my-exwm-update-title ()
-  (pcase exwm-class-name
-    ("Firefox" (exwm-workspace-rename-buffer (format "Firefox: %s" exwm-title)))))
+  (exwm-workspace-rename-buffer exwm-class-name))
 
 (defun my-exwm-update-screen ()
   (message "Resizing screen...")  ;; Do not remove. Resize does not work without this line :P
@@ -12,7 +11,7 @@
   (message "Done."))
 
 (add-hook 'exwm-randr-screen-change-hook #'my-exwm-update-screen)
-(add-hook 'exwm-update-title-hook #'my-exwm-update-title)
+(add-hook 'exwm-update-class-hook #'my-exwm-update-title)
 
 (exwm-input-set-key (kbd "M-<right>") 'tab-line-switch-to-next-tab)
 (exwm-input-set-key (kbd "M-<left>") 'tab-line-switch-to-prev-tab)
