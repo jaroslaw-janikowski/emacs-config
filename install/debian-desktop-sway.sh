c630cc5e9f22df5f0abfefa5b83774b76228749d
JJ <jaroslaw.janikowski@gmail.com>
2025-10-22 12:13:34 +0200

    Oddzielenie instalacji sway od instalacji narzędzi programistycznych.

---
+++ install/debian-desktop-sway.sh
@@ -0,0 +1,55 @@
+# aktualizacja
+apt update -y
+apt upgrade -y
+apt autoremove -y
+
+# Sway desktop
+apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock grimshot wdisplays p7zip-full clangd gnupg2 unifont silversearcher-ag gdb clzip sqlite3 sqlite3-doc htop
+
+# narzędzia
+apt install -y mc
+
+# nagrywanie ekranu w Wayland
+apt install -y wf-recorder
+
+# spelling tools
+apt install -y aspell aspell-pl aspell-en
+
+# sway setup
+su - work -- <<EOF
+mkdir ~/.config/sway
+cp /etc/sway/config ~/.config/sway/config
+cat <<EOF2>> ~/.config/sway/config
+
+exec brightnessctl set 6%
+
+input * {
+    xkb_layout "pl"
+}
+
+xwayland enable
+bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +10%
+bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -10%
+bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle
+bindsym XF86MonBrightnessUp exec brightnessctl set 3%+
+bindsym XF86MonBrightnessDown exec brightnessctl set 3%-
+bindsym Print exec grimshot copy area
+workspace_layout tabbed
+
+EOF2
+
+sed -i "s/position top/position bottom/" ~/.config/sway/config
+sed -i "s/%I:%M:%S/%H:%M:%S/" ~/.config/sway/config
+sed -i "s/| dmenu | /| dmenu -m 0 | /" ~/.config/sway/config
+EOF
+
+cat <<EOF>> ~/.profile
+
+sway &
+
+EOF
+
+apt install -y firefox-esr
+
+# spelling tools
+apt install -y aspell aspell-pl aspell-en
--- install/setup-debian-work.sh
+++ install/setup-debian-work.sh
@@ -1,64 +1,12 @@
-# aktualizacja
-apt update -y
-apt upgrade -y
-apt autoremove -y
+# instalacja pożądanego pulpitu
+source ./debian-desktop-sway.sh
 
-# Sway desktop
-apt install -y sway make xwayland pavucontrol brightnessctl swayidle swaylock grimshot wdisplays p7zip-full clangd gnupg2 unifont silversearcher-ag gdb clzip sqlite3 sqlite3-doc htop
-
-# narzędzia
-apt install -y mc
-
-# nagrywanie ekranu w Wayland
-apt install -y wf-recorder
-
-# spelling tools
-apt install -y aspell aspell-pl aspell-en
-
-# sway setup
-su - work -- <<EOF
-mkdir ~/.config/sway
-cp /etc/sway/config ~/.config/sway/config
-cat <<EOF2>> ~/.config/sway/config
-
-exec brightnessctl set 6%
-
-input * {
-    xkb_layout "pl"
-}
-
-xwayland enable
-bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +10%
-bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -10%
-bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle
-bindsym XF86MonBrightnessUp exec brightnessctl set 3%+
-bindsym XF86MonBrightnessDown exec brightnessctl set 3%-
-bindsym Print exec grimshot copy area
-workspace_layout tabbed
-
-EOF2
-
-sed -i "s/position top/position bottom/" ~/.config/sway/config
-sed -i "s/%I:%M:%S/%H:%M:%S/" ~/.config/sway/config
-sed -i "s/| dmenu | /| dmenu -m 0 | /" ~/.config/sway/config
-EOF
-
-cat <<EOF>> ~/.profile
-
-sway &
-
-EOF
-
-# przeglądarki internetowe do webdevu
+# Google Chrome
 su - work -- <<EOF
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 EOF
 apt install -y /tmp/google-chrome-stable_current_amd64.deb
-apt install -y firefox-esr
-
-# spelling tools
-apt install -y aspell aspell-pl aspell-en
 
 # git setup
 apt install -y git git-gui
