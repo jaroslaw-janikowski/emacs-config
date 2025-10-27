78e2c8dcf55100d34cc92c92d6ef78f854fa80c7
JJ <jaroslaw.janikowski@gmail.com>
2025-10-27 12:53:47 +0100

    Dodanie serwera LSP.

--- install/devbox-python.sh
+++ install/devbox-python.sh
@@ -1,2 +1,2 @@
 # normalny serwer deweloperski
-apt install -y git nginx nginx-doc postgresql postgresql-doc python3 python3-doc python3-psycopg2 python-psycopg2-doc python3-django python-django-doc python3-djangorestframework python-djangorestframework-doc celery python3-celery python-celery-doc rabbitmq-server ssh-server npm firefox-esr
+apt install -y git nginx nginx-doc postgresql postgresql-doc python3 python3-doc python3-psycopg2 python-psycopg2-doc python3-django python-django-doc python3-djangorestframework python-djangorestframework-doc celery python3-celery python-celery-doc rabbitmq-server ssh-server npm firefox-esr python3-pylsp
